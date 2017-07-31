12b52b81cd7132e9d8344592fef1c98a66ea7267
Bill Donner <billdonner@gmail.com>
2017-07-31 13:06:42 -0400

    Initial Commit

---
+++ cougher WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  cougher WatchKit Extension
+//
+//  Created by bill donner on 7/31/17.
+//  Copyright © 2017 billdonner. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ cougher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cougher
+//
+//  Created by bill donner on 7/31/17.
+//  Copyright © 2017 billdonner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
